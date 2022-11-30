38f425d4335e422ab47415bde56ac205a86c34f3
BenHbrt <benherbert1983@googlemail.com>
2022-11-30 21:30:40 +0100

    Randomised dates

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -13,8 +13,8 @@ const Naughty = ({ setDisplay }) => {
             <p>I wrote this app as part of my coding portfolio, which can be found at <a href="https://github.com/BenHbrt">https://github.com/BenHbrt</a>.</p>
             <p>The inspiration for this project came as a result of brainstorming app ideas with a friend of mine, Renee Selikowitz, over breakfast, as well as my background in English langauge teaching.</p>
             <p>The images used in this project were taken from <a href="https://pixabay.com/">pixabay.com</a>, and direct links to them can be found below.</p>
-                <a href="https://pixabay.com/photos/christmas-background-flat-lay-1911637/"><img src={require("../Images/hd-wallpaper-g96f4c3791_1280.jpg")} alt="A christmas image" /></a>
-                <a href="https://pixabay.com/vectors/holly-ilex-leaves-thorny-spiky-152061/"><img src={require("../Images/holly-g5eb13151e_640.png")} alt="A christmas image" /></a>
+                <a href="https://pixabay.com/photos/christmas-background-flat-lay-1911637/"><img src={require("../Images/hd-wallpaper-g96f4c3791_1280.jpg")} alt="Christmas" /></a>
+                <a href="https://pixabay.com/vectors/holly-ilex-leaves-thorny-spiky-152061/"><img src={require("../Images/holly-g5eb13151e_640.png")} alt="Holly" /></a>
             <p>The icons and 'Mountains of Christmas' font used in this project (featured below) were taken from <a href="https://fonts.google.com/">fonts.google.com</a>.</p>
             <div className="credits_icons">
                 <img src={require('../Images/calendar_month_FILL0_wght400_GRAD0_opsz48.png')} alt="Calendar icon"></img>
--- src/DataAndFunctions/DayData.js
+++ src/DataAndFunctions/DayData.js
@@ -1,5 +1,42 @@
 const dayData = [
     {
+        date: 14,
+        text: "TEXT14"
+    },
+    {
+        date: 9,
+        text: "TEXT9"
+    },
+    {
+        date: 24,
+        text: "You've almost made it. Have a merry Christmas tomorrow!"
+    },   
+    {
+        date: 7,
+        text: "TEXT7"
+    },   
+    {
+        date: 13,
+        text: "TEXT13"
+    },
+    {
+        date: 21,
+        text: "TEXT21"
+    },
+    {
+        date: 18,
+        text: "TEXT18"
+    },
+    {
+        date: 20,
+        text: "TEXT20"
+    },
+    {
+        date: 15,
+        text: "TEXT15",
+        image: "mistletoe2.png"
+    },
+    {
         date: 1,
         data: {
             phoneme: {  
@@ -21,16 +58,16 @@ const dayData = [
         
     },
     {
-        date: 2,
-        data: {
-            phoneme: {symbol: "ʊ"}
-        }
+        date: 23,
+        text: "TEXT23"
     },
     {
-        date: 3,
-        data: {
-            phoneme: {symbol: "ŋ"}
-        }
+        date: 16,
+        text: "TEXT16"
+    },
+    {
+        date: 22,
+        text: "TEXT22"
     },
     {
         date: 4,
@@ -39,9 +76,28 @@ const dayData = [
         }
     },
     {
-        date: 5,
+        date: 8,
+        text: "TEXT8",
+        image: "gingerbread-man.png"
+    },
+    {
+        date: 2,
         data: {
-            phoneme: {symbol: "ə"}
+            phoneme: {symbol: "ʊ"}
+        }
+    },
+    {
+        date: 11,
+        text: "TEXT11"
+    },
+    {
+        date: 17,
+        text: "TEXT17"
+    },
+    {
+        date: 3,
+        data: {
+            phoneme: {symbol: "ŋ"}
         }
     },
     {
@@ -51,79 +107,23 @@ const dayData = [
         }
     },
     {
-        date: 7,
-        text: "TEXT7"
-    },
-    {
-        date: 8,
-        text: "TEXT8",
-        image: "gingerbread-man.png"
-    },
-    {
-        date: 9,
-        text: "TEXT9"
-    },
-    {
         date: 10,
         text: "TEXT10",
         image: "bauble.png"
-    },
-    {
-        date: 11,
-        text: "TEXT11"
     },
     {
         date: 12,
         text: "TEXT12"
     },
     {
-        date: 13,
-        text: "TEXT13"
-    },
-    {
-        date: 14,
-        text: "TEXT14"
-    },
-    {
-        date: 15,
-        text: "TEXT15",
-        image: "mistletoe2.png"
-    },
-    {
-        date: 16,
-        text: "TEXT16"
-    },
-    {
-        date: 17,
-        text: "TEXT17"
-    },
-    {
-        date: 18,
-        text: "TEXT18"
+        date: 5,
+        data: {
+            phoneme: {symbol: "ə"}
+        }
     },
     {
         date: 19,
         text: "TEXT19"
     },
-    {
-        date: 20,
-        text: "TEXT20"
-    },
-    {
-        date: 21,
-        text: "TEXT21"
-    },
-    {
-        date: 22,
-        text: "TEXT22"
-    },
-    {
-        date: 23,
-        text: "TEXT23"
-    },
-    {
-        date: 24,
-        text: "You've almost made it. Have a merry Christmas tomorrow!"
-    }
 ]
 export default dayData;
