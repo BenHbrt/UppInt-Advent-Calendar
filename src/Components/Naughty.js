6625efa08ee430ad28e2a900c1b328ad437c6b6c
BenHbrt <benherbert1983@googlemail.com>
2022-11-30 19:34:02 +0100

    Naughty incl styling

--- src/Components/Naughty.js
+++ src/Components/Naughty.js
@@ -4,8 +4,11 @@ import CloseButton from './CloseButton';
 const Naughty = ({ setDisplay }) => {
     return (
         <div className="naughty">
-            <h2>Naughty, naughty! You have to wait to open this one!</h2>
             <CloseButton func={() => setDisplay("Calendar")} />
+            <div className='naughty_text'>
+                <span className="naughty_text_first">Naughty, naughty!</span>
+                <span>You have to wait to open this one!</span>
+            </div>
             <img className="TR" src={require('../Images/holly-g5eb13151e_640.png')} alt=""/>
             <img className="BR" src={require('../Images/holly-g5eb13151e_640.png')} alt=""/>
             <img className="BL" src={require('../Images/holly-g5eb13151e_640.png')} alt=""/>
